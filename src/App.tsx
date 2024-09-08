a117b633c31f79c237a53c1ba1e6ec42264c4006
Manav Chawla <chawlamanav428@gmail.com>
2024-09-08 12:28:03 +0530

    proj ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ServicesNew from "./pages/ServicesNew";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
